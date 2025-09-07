a6eb7e64a1e0b0937bdec1a773eede7d4e642d85
kazin-dima@mail.ru <kazin-dima@mail.ru>
2025-09-07 23:05:31 +0300

    Исправлены мелкие баги

--- scripts/start_button.gd
+++ scripts/start_button.gd
@@ -2,7 +2,7 @@ extends Button
 
 
 func _on_pressed() -> void:
-	get_tree().change_scene_to_file("res://levels/first_floor.tscn")
+	get_tree().change_scene_to_file("res://levels/second_floor.tscn")
 
 
 func _on_exit_button_pressed() -> void:
